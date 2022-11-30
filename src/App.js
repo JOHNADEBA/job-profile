c5a94e47798c3fe5811af6c3bb8af265ea54876f
john.adeba <John.Adeba@endava.com>
2022-11-30 11:26:21 +0100

    first build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+import "./App.css";
+import data from "./MOCK_DATA.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [index, setIndex] = useState(0);
+	const { name, role, start_date, end_date, description } = data[index];
+	console.log(data);
+
+	const convertDate = (value) =>
+		new Date(parseInt(value)).toLocaleDateString("en-SL", {
+			year: "numeric",
+			month: "short",
+		});
+
+	const getUser = (id) => {
+		setIndex(id - 1);
+	};
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="App-logo">Experience</h1>
+				<div className="border"></div>
+			</header>
+			<main>
+				<div className="nav-cont">
+					{data.map((menu) => {
+						return (
+							<nav key={menu.id}>
+								<button
+									onClick={() => getUser(menu.id)}
+									className={index + 1 === menu.id && "active_btn"}
+								>
+									{menu.name}
+								</button>
+							</nav>
+						);
+					})}
+				</div>
+
+				<div className="right-cont">
+					<p className="role">{role}</p>
+					<p className="name">{name}</p>
+					<p className="date">
+						{convertDate(start_date)} - {convertDate(end_date)}
+					</p>
+					{description.map((item, index) => {
+						return (
+							<div className="skills" key={index}>
+								<p className="icon">
+									<FaAngleDoubleRight />
+								</p>
+								<p className="skill-item">{item}</p>
+							</div>
+						);
+					})}
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
